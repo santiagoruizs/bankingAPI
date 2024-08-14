ef2fb6cf5e40942c1e25b05b2c092272457ecd9d
Santiago Ruiz <sruizs@Santiagos-Laptop.local>
2024-08-14 19:59:01 +0200

    Adds transfer routes and check for balance middleware for transactions

--- models/Account.js
+++ models/Account.js
@@ -1,53 +1,95 @@
-const {pool} = require('../config/db');
-
+const { pool } = require("../config/db");
 
 // Account queries
 async function createAccount(userId) {
-    let accountNumber = '';
-    for (let i = 0; i < 8; i++) {
-        // Generate a random digit between 0 and 9 and append it to the account number
-        accountNumber += Math.floor(Math.random() * 10);
-    }
-    const query = 'INSERT INTO accounts (account_number, user_id, balance) VALUES ($1, $2, $3) RETURNING account_number, balance';
-    const values = [accountNumber, userId, 2000];
-    const res = await pool.query(query, values);
-    return res.rows[0];
+  let accountNumber = "";
+  for (let i = 0; i < 8; i++) {
+    // Generate a random digit between 0 and 9 and append it to the account number
+    accountNumber += Math.floor(Math.random() * 10);
+  }
+  const query =
+    "INSERT INTO accounts (account_number, user_id, balance) VALUES ($1, $2, $3) RETURNING account_number, balance";
+  const values = [accountNumber, userId, 2000];
+  const res = await pool.query(query, values);
+  return res.rows[0];
 }
 
 async function getAccountInfoByUserId(userId) {
-  const query = 'SELECT * FROM accounts WHERE user_id = $1';
+  const query = "SELECT * FROM accounts WHERE user_id = $1";
   const values = [userId];
   const res = await pool.query(query, values);
   return res.rows[0];
 }
 
 async function getAccountInfoByAccountNumber(acountNumber) {
-    const query = 'SELECT * FROM accounts WHERE account_number = $1';
-    const values = [acountNumber];
-    const res = await pool.query(query, values);
-    return res.rows[0];
-  }
+  const query = "SELECT * FROM accounts WHERE account_number = $1";
+  const values = [acountNumber];
+  const res = await pool.query(query, values);
+  return res.rows[0];
+}
 
-// async function transferAccountFunds(acountNumber1, accountNumber2) {
-//   const query = 'SELECT id, username FROM users WHERE id = $1';
-//   const values = [id];
-//   const res = await pool.query(query, values);
-//   return res.rows[0];
-// }
+async function checkSufficientBalance(user_id, amount) {
+  const query = "SELECT balance FROM accounts WHERE user_id = $1";
+  const values = [user_id];
+  const res = await pool.query(query, values);
+  return res.rows[0].balance > amount;
+}
 
-async function addFundsToAccount (user_id, amount) {
-  const query = 'UPDATE accounts set balance = balance + $1 WHERE user_id = $2 RETURNING balance';
+async function transferAccountFunds(user_id, toAccountNumber, amount) {
+  try {
+    const parsedAmount = parseFloat(amount)
+    console.log(amount)
+    console.log(toAccountNumber)
+    await pool.query("BEGIN");
+
+    const subtractFundsQuery = `
+      UPDATE accounts
+      SET balance = balance - $1
+      WHERE user_id = $2
+      RETURNING balance;
+    `;
+    const subtractValues = [parsedAmount, user_id];
+    const subtractRes = await pool.query(subtractFundsQuery, subtractValues);
+    console.log(subtractRes.rows)
+    const addFundsQuery = `
+    UPDATE accounts
+    SET balance = balance + $1
+    WHERE account_number = $2
+    RETURNING balance;
+    `;
+    const addValues = [parsedAmount, toAccountNumber];
+    const addRes = await pool.query(addFundsQuery, addValues);
+    console.log(addRes.rows)
+    if (addRes.rows.length === 0) {
+      throw new Error("Destination account not found");
+    }
+    await pool.query("COMMIT");
+
+    return {
+      fromAccountBalance: subtractRes.rows[0].balance,
+    };
+  } catch (error) {
+    await pool.query("ROLLBACK");
+    throw error;
+  } 
+}
+
+async function addFundsToAccount(user_id, amount) {
+  const query =
+    "UPDATE accounts set balance = balance + $1 WHERE user_id = $2 RETURNING balance";
   const values = [amount, user_id];
   const res = await pool.query(query, values);
   return res.rows[0];
 }
 
 async function widthdrawFunds(user_id, amount) {
-  const query = 'UPDATE accounts set balance = balance - $1 WHERE user_id = $2 RETURNING balance';
+  const query =
+    "UPDATE accounts set balance = balance - $1 WHERE user_id = $2 RETURNING balance";
   const values = [amount, user_id];
   const res = await pool.query(query, values);
   return res.rows[0];
 }
+
 // async function deleteAccount(username) {
 //     const query = 'UPDATE users set attempts = 0 WHERE username = $1 RETURNING attempts';
 //     const values = [username];
@@ -60,6 +102,7 @@ module.exports = {
   getAccountInfoByUserId,
   getAccountInfoByAccountNumber,
   addFundsToAccount,
-  widthdrawFunds
-
+  widthdrawFunds,
+  checkSufficientBalance,
+  transferAccountFunds
 };
--- routes/account.js
+++ routes/account.js
@@ -1,7 +1,19 @@
 const express = require('express');
 const passport = require('passport');
-const { addFundsToAccount, widthdrawFunds, getAccountInfoByUserId } = require('../models/Account');
+const { addFundsToAccount, widthdrawFunds, getAccountInfoByUserId, checkSufficientBalance, transferAccountFunds } = require('../models/Account');
 const router = express.Router();
+
+const checkBalance = async (req, res, next) => {
+    const user_id = req.params.user_id
+    const {ammount} = req.body
+    const sufficientBalance = await checkSufficientBalance(user_id, ammount)
+    if(sufficientBalance){
+        next()
+    }else{
+        res.status(400).json({message : 'Not Enough funds'})
+    }
+
+}
 
 //Get Account Info
 router.get('/:user_id', async (req, res) => {
@@ -10,7 +22,7 @@ router.get('/:user_id', async (req, res) => {
         const account = await getAccountInfoByUserId(user_id)
         res.status(200).json(account);
     }catch(err){
-        return res.status(500).json({ message: 'Server error' });
+        return res.status(500).json({ message: err.message });
     }   
 });
 // deposit
@@ -23,12 +35,12 @@ router.put('/:user_id/deposit', async (req, res) => {
         const newBalance = await addFundsToAccount(user_id, parsedAmount)
         res.status(200).json({ message: 'Funds added', balance: newBalance });
     }catch(err){
-        return res.status(500).json({ message: 'Server error' });
+        return res.status(500).json({ message: err.message });
     }   
 });
 
 // widthdraw
-router.put('/:user_id/widthdraw', async (req, res) => {
+router.put('/:user_id/widthdraw', checkBalance, async (req, res) => {
     const user_id = req.params.user_id
     const {ammount} = req.body
     const parsedAmount = parseFloat(ammount)
@@ -36,10 +48,23 @@ router.put('/:user_id/widthdraw', async (req, res) => {
         const newBalance = await widthdrawFunds(user_id, parsedAmount)
         res.status(200).json({ message: 'Funds added', balance: newBalance });
     }catch(err){
-        return res.status(500).json({ message: 'Server error' });
+        return res.status(500).json({ message: err.message });
     }  
 });
 
+// transfer
+router.put('/:user_id/transfer', checkBalance, async (req, res) => {
+    const user_id = req.params.user_id
+    console.log(req.body)
+    const {ammount, toAccountNumber} = req.body
+    const parsedAmount = parseFloat(ammount)
+    try{
+        const newBalance = await transferAccountFunds(user_id, toAccountNumber, parsedAmount)
+        res.status(200).json({ message: 'Transfer done succesfully', balance: newBalance });
+    }catch(err){
+        return res.status(500).json({ message: err.message });
+    }  
+});
 
 
 module.exports = router;
