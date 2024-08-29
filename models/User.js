f0d37daf9153a755a925b1be421239d68d832834
Santiago Ruiz <sruizs@Santiagos-Laptop.local>
2024-08-29 19:55:26 +0200

    General bug fixes

--- models/User.js
+++ models/User.js
@@ -5,35 +5,35 @@ const bcrypt = require('bcryptjs');
 async function createUser(username, password, email) {
   const salt = await bcrypt.genSalt(10);
   const hashedPassword = await bcrypt.hash(password, salt);
-  const query = 'INSERT INTO users (username, password, email, attempts) VALUES ($1, $2, $3, 0) RETURNING id, username';
+  const query = 'INSERT INTO public.users (username, password, email, attempts) VALUES ($1, $2, $3, 0) RETURNING id, username';
   const values = [username, hashedPassword, email];
   const res = await pool.query(query, values);
   return res.rows[0];
 }
 
 async function findUserByUsername(username) {
-  const query = 'SELECT * FROM users WHERE username = $1';
+  const query = 'SELECT * FROM public.users WHERE username = $1';
   const values = [username];
   const res = await pool.query(query, values);
   return res.rows[0];
 } 
 
 async function findUserById(id) {
-  const query = 'SELECT id, username FROM users WHERE id = $1';
+  const query = 'SELECT id, username FROM public.users WHERE id = $1';
   const values = [id];
   const res = await pool.query(query, values);
   return res.rows[0];
 }
 
 async function addLoginAttempt(username) {
-  const query = 'UPDATE users set attempts = attempts + 1 WHERE username = $1 RETURNING attempts';
+  const query = 'UPDATE public.users set attempts = attempts + 1 WHERE username = $1 RETURNING attempts';
   const values = [username];
   const res = await pool.query(query, values);
   return res.rows[0];
 }
 
 async function resetLoginAttempts(username) {
-  const query = 'UPDATE users set attempts = 0 WHERE username = $1 RETURNING attempts';
+  const query = 'UPDATE public.users set attempts = 0 WHERE username = $1 RETURNING attempts';
   const values = [username];
   const res = await pool.query(query, values);
   return res.rows[0];
